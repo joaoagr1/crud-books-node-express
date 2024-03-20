dee99715c468531016887e01ecdddb21fd7bd37d
João Antonio Garcia Rolo <joaorolo@usacucar.com.br>
2024-03-20 08:58:10 -0300

    :bug: fix: Resolvendo bug da associação de autores e livros

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import { routes } from './routes'
+import { routes } from '../routes/routes'
 
 class App {
     express: express.Application
